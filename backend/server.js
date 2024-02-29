be60638ab3c3b6075362a8f2d344f6acebd4e8f2
SuperNek <nikita.orehov333@gmail.com>
2024-02-29 20:39:18 +0300

    init

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    // root route http://localhost:3000
+    res.send("Hello World!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
