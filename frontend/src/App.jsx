88018975b71eb53de96a6a50ef699fd161c0e7af
SuperNek <nikita.orehov333@gmail.com>
2024-03-11 08:55:10 +0300

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import Register from "./components/Register"
-=======
 import Login from "./pages/login/Login"
 import Home from "./pages/Home/Home"
 import Register from "./pages/register/Register"
->>>>>>> 60b6f6ab0a3c621b5ce921cb6b400abf3f83bc55
 
 function App() {
 
